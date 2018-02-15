c5486c135545da3cd1b7b71a833acf21a2f3aed6
Gary Mendonca <mendonca_gary@ymail.com>
2018-02-15 12:15:30 +0530

    Removed unnecessary links

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -15,11 +15,11 @@ constructor(props) {
  };
 
  //binding all our functions to this class
- this.deleteProduct = this.deleteProduct.bind(this);
- this.updateProduct = this.updateProduct.bind(this);
- this.handleNameChange = this.handleNameChange.bind(this);
- this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
- this.handleProductUpdate = this.handleProductUpdate.bind(this);
+ //this.deleteProduct = this.deleteProduct.bind(this);
+ //this.updateProduct = this.updateProduct.bind(this);
+ //this.handleNameChange = this.handleNameChange.bind(this);
+ //this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+ //this.handleProductUpdate = this.handleProductUpdate.bind(this);
  this.openProductSheet = this.openProductSheet.bind(this);
  }
 
@@ -38,42 +38,42 @@ let price = this.props.price;
  console.log('oops deleted');
  }
  
- handleProductUpdate(e) {
- e.preventDefault();
- let id = this.props.uniqueID;
- //if name or description changed, set it. if not, leave null and our PUT 
- //request will ignore it.
- let name = (this.state.name) ? this.state.name : null;
- let description = (this.state.description) ? this.state.description : null;
- let price = (this.state.price) ? this.state.price : null;
- let product = { name: name, description: description, price: price};
- this.props.onProductUpdate(id, product);
- this.setState({
- toBeUpdated: !this.state.toBeUpdated,
- name: '',
- description: '',
- price: ''
- })
- }
+//  handleProductUpdate(e) {
+//  e.preventDefault();
+//  let id = this.props.uniqueID;
+//  //if name or description changed, set it. if not, leave null and our PUT 
+//  //request will ignore it.
+//  let name = (this.state.name) ? this.state.name : null;
+//  let description = (this.state.description) ? this.state.description : null;
+//  let price = (this.state.price) ? this.state.price : null;
+//  let product = { name: name, description: description, price: price};
+//  this.props.onProductUpdate(id, product);
+//  this.setState({
+//  toBeUpdated: !this.state.toBeUpdated,
+//  name: '',
+//  description: '',
+//  price: ''
+//  })
+//  }
 
- deleteProduct(e) {
- e.preventDefault();
- let id = this.props.uniqueID;
- this.props.onProductDelete(id);
- console.log('oops deleted');
- }
+//  deleteProduct(e) {
+//  e.preventDefault();
+//  let id = this.props.uniqueID;
+//  this.props.onProductDelete(id);
+//  console.log('oops deleted');
+//  }
 
- handleDescriptionChange(e) {
- this.setState({ description: e.target.value });
- }
+//  handleDescriptionChange(e) {
+//  this.setState({ description: e.target.value });
+//  }
 
- handleNameChange(e) {
- this.setState({ name: e.target.value });
- }
+//  handleNameChange(e) {
+//  this.setState({ name: e.target.value });
+//  }
  
- handlePriceChange(e) {
-    this.setState({ price: e.target.value });
-    }
+//  handlePriceChange(e) {
+//     this.setState({ price: e.target.value });
+//     }
  
 rawMarkup() {
  let rawMarkup = marked(this.props.children.toString());
@@ -85,12 +85,12 @@ rawMarkup() {
  <div style={ style.comment }>
  <h3>{this.props.name}</h3>
  <span dangerouslySetInnerHTML={ this.rawMarkup() } />
- <a style={ style.updateLink } href='#' onClick={ this.updateProduct }>update</a>
- <a style={ style.deleteLink } href='#' onClick={ this.deleteProduct }>delete</a>
+ {/* <a style={ style.updateLink } href='#' onClick={ this.updateProduct }>update</a>
+ <a style={ style.deleteLink } href='#' onClick={ this.deleteProduct }>delete</a> */}
  <a style={ style.updateLink } href='#' onClick={ this.openProductSheet }>buy</a>
- { (this.state.toBeUpdated)
- ? (<form onSubmit={ this.handleProductUpdate }>
- <input
+ {/* { (this.state.toBeUpdated)
+ ? (<form onSubmit={ this.handleProductUpdate }> */}
+ {/* <input
  type='text'
  placeholder='Update name…'
  style={ style.commentFormAuthor }
@@ -113,7 +113,7 @@ rawMarkup() {
  style={ style.commentFormPost }
  value='Update' />
  </form>)
- : null}
+ : null} */}
  </div>
  )
  }
